213316e5172eb910499fd041b19508652895b011
bhumika sahu <bhumi16124@gmail.com>
2026-02-26 14:10:28 +0530

    commit the code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.use('/ai', aiRoutes);
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import Editor from "react-simple-code-editor"
 import prism from "prismjs"
-import Markdown from "react-markdown"
+import Markdown from "react-markdown" 
 import rehypeHighlight from "rehype-highlight"
 import "highlight.js/styles/github-dark.css"
 import axios from 'axios'
